c880425822a20973e4439654345807db3797f830
HAEiNEe <jhi5576@naver.com>
2021-01-18 20:32:50 +0900

    fixed

--- src/api/User/createAccont/createAccont.js
+++ src/api/User/createAccont/createAccont.js
@@ -3,7 +3,13 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
     Mutation: {
         createAccount: async (_, args) => {
-            const { username, email, firstName, lastName, bio } = args;
+            const { username, email, firstName = "", lastName = "", bio = "" } = args;
+            const existsusername = await prisma.$exists.user({ username });
+            const existemail = await prisma.$exists.user({ email });
+            if (existsusername || existemail) {
+                throw Error("이미 사용중인 아이디/email 입니다.");
+                return;
+            }
             await prisma.createUser({
                 username,
                 email,
